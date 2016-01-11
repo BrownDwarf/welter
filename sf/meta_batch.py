688307ec03f6ae7061650fddb117a43006558c0d
gully <igully@gmail.com>
2016-01-11 15:45:11 +0800

    Closes Issue #9-- generate synthetic data.  Now done for ESPaDOnS and IGRINS

--- sf/meta_batch.py
+++ sf/meta_batch.py
@@ -19,17 +19,17 @@ for mdir in mdirs:
     print(os.getcwd())
     print(' ')
 
-    print("Running startup1.sh: ")    
-    print("startup1.sh")
-    os.system('startup1.sh')
-    print("Completed startup1.sh")
-    print(' ')
+    #print("Running startup1.sh: ")    
+    #print("startup1.sh")
+    #os.system('startup1.sh')
+    #print("Completed startup1.sh")
+    #print(' ')
 
     print("Starting the MCMC run.")
-    out_dir = "output/LkCa4_{}/run01/".format(mdir)
+    out_dir = "output/LkCa4_{}/run02/".format(mdir)
     print("os.chdir(out_dir)")
     print("Spawning a screen")
-    screen_command = 'screen -d -m bash -c ' + '"cd /home/gully/GitHub/welter/sf/{}/output/LkCa4_{}/run01/'.format(mdir, mdir)+'; run.sh"' 
+    screen_command = 'screen -d -m bash -c ' + '"cd /home/gully/GitHub/welter/sf/{}/output/LkCa4_{}/run02/'.format(mdir, mdir)+'; run.sh"' 
     print(screen_command)
     os.system(screen_command)
     print(' ')
