5b6c6d49e23726a580ed26e130f8560bc01d3ee0
gully <igully@gmail.com>
2016-06-20 13:28:45 +0800

    Batch scripts for making plots; ignore the rsync batch script for now.

---
+++ code/batch_plot_mix.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+import yaml
+import numpy as np 
+import h5py
+
+ms = range(99, 119)
+#ms = range(72, 94+1)
+
+os.chdir(os.path.expandvars('$WELTER/sf/'))
+
+os.getcwd()
+
+for m in ms:
+
+    dat_name = '../../data/reduced/LkCa4_IGRINS_m{:03d}.hdf5'.format(m)
+    path_out = 'm{:03d}/output/mix_emcee/run01'.format(m)
+    os.chdir(path_out)
+
+    print('Plotting results from m{:03d}'.format(m))
+    # Do all the grid.py and pca.py setup.
+    os.system('plot_many_mix_models.py --static')
+    os.system('cp mix_model.png '+
+    	os.path.expandvars('$WELTER/results/fig/mix_models/mix_model_m{:03d}.png'.format(m)))
+    os.chdir(os.path.expandvars('$WELTER/sf/'))
