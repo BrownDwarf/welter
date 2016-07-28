b6b0a64e28554fc032116174065f796d9ca9c9c9
gully <igully@gmail.com>
2016-07-28 12:03:47 +0800

    spot check the most recent round of Starfish MCMC runs, orders 100-117

--- code/batch_plot_mix.py
+++ code/batch_plot_mix.py
@@ -4,9 +4,9 @@ import yaml
 import numpy as np 
 import h5py
 
-#ms = range(99, 119)
+ms = range(100, 117)
 #ms = range(72, 94+1)
-ms = list(range(72, 94+1)) + list(range(99, 119))
+#ms = list(range(72, 94+1)) + list(range(99, 119))
 
 os.chdir(os.path.expandvars('$WELTER/sf/'))
 
@@ -14,13 +14,12 @@ os.getcwd()
 
 for m in ms:
 
-    dat_name = '../../data/reduced/LkCa4_IGRINS_m{:03d}.hdf5'.format(m)
-    path_out = 'm{:03d}/output/mix_emcee/run01'.format(m)
+    path_out = 'm{:03d}/output/mix_emcee/run02'.format(m)
     os.chdir(path_out)
 
     print('Plotting results from m{:03d}'.format(m))
-    # Do all the grid.py and pca.py setup.
+    os.system('cp temp_emcee_chain.npy emcee_chain.npy')
     os.system('plot_many_mix_models.py --static')
     os.system('cp mix_model.png '+
-    	os.path.expandvars('$WELTER/results/fig/mix_models/mix_model_m{:03d}.png'.format(m)))
+    	os.path.expandvars('$WELTER/results/fig/mix_models_run02/mix_model_m{:03d}.png'.format(m)))
     os.chdir(os.path.expandvars('$WELTER/sf/'))
